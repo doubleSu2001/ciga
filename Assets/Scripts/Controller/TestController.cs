5a62017f567cd94c738032d6079408480debfdf0
dljgs1 <906348668@qq.com>
2021-07-07 20:21:50 +0800

    项目设置 & 测试demo

---
+++ Assets/Scripts/Controller/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestController : MonoBehaviour
+{
+    SpriteRenderer sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, (1+Mathf.Sin(Time.time))/2);
+    }
+}
---
+++ Assets/Scripts/Editor/TableInitializer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+#if UNITY_EDITOR
+/// <summary>
+/// 数据表初始化
+/// </summary>
+public class TableInitializer : AssetPostprocessor
+{
+    static void GeneratePuzzleTable(string csvfile, string toSaveFile)
+    {
+        // TextAsset data = AssetDatabase.LoadAssetAtPath<TextAsset>(csvfile);
+        // PuzzleTable gm = ScriptableObject.CreateInstance<PuzzleTable>();
+        //Debug.Log(data.text);
+        // gm.mItems = CSVSerializer.Deserialize<PuzzleTable.Meta>(data.text);
+        // AssetDatabase.CreateAsset(gm, toSaveFile);
+        // AssetDatabase.SaveAssets();
+        
+    }
+
+    static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+    {
+        //Debug.Log("start");
+        // 物品表
+        // GeneratePuzzleTable("Assets/Table/PuzzleTable.csv", "Assets/Resources/Table/PuzzleTable.asset");
+
+    }
+}
+#endif
