70f7675a1d8983925568c5da7d47218d50f6fa4b
dljgs1 <906348668@qq.com>
2021-07-11 19:56:39 +0800

    开始界面添加

---
+++ Assets/Scripts/UI/StartGame.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public Image [] StudyUI;
+    [Header("开始学习UI判断")]
+    private bool isStudyUI;
+    [Header("学习UI的页数控制")]
+    private int studyIndex=0;
+
+    // Start is called before the first frame update
+    void Start()
+    {      
+
+    }
+    public void StudyUI1()
+    {
+        isStudyUI = true;
+        StudyUI[0].gameObject.SetActive(true);
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)&&isStudyUI)
+        {
+            StudyUI[studyIndex].gameObject.SetActive(true);
+            studyIndex += 1;
+        }
+        if (studyIndex >= 2)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("转换场景");
+                SceneManager.LoadScene(1);
+            }
+        }
+    }
+}
